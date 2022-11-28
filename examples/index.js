bd93a406aa3362333e757bea0c176bee3f2e425d
ilham8944 <ilham_8944nugraha@apps.ipb.ac.id>
2022-11-28 13:22:33 +0700

    tes

---
+++ examples/index.js
@@ -0,0 +1,29 @@
+var map = L.map('map');
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var control = L.Routing.control(L.extend(window.lrmConfig, {
+	waypoints: [
+		L.latLng(-6.589177430577552, 106.80611563247919),
+		L.latLng(-6.588499440337558, 106.80529413520688)
+	],
+	geocoder: L.Control.Geocoder.nominatim(),
+	routeWhileDragging: true,
+	reverseWaypoints: true,
+	showAlternatives: true,
+	altLineOptions: {
+		styles: [
+			{color: 'black', opacity: 0.15, weight: 9},
+			{color: 'white', opacity: 0.8, weight: 6},
+			{color: 'blue', opacity: 0.5, weight: 2}
+		]
+	}
+})).addTo(map);
+
+L.marker([-6.589300802735809, 106.80514714336628]).addTo(map)
+    .bindPopup('Roofpark Cafe & Restaurant Bogor')
+    .openPopup();
+
+L.Routing.errorControl(control).addTo(map);
